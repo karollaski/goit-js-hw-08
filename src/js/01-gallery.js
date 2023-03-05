565b41ba0792376a40b45f5ee3589e066e53e73d
karollaski <karol.laski994@gmail.com>
2023-03-05 16:03:08 +0100

    task 01 finished

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,30 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+// Opisany w dokumentacji
+import SimpleLightbox from 'simplelightbox';
+// Dodatkowy import stylów
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-console.log(galleryItems);
+const galleryWrap = document.querySelector('.gallery');
+const galleryCollection = createGallery(galleryItems);
+
+function createGallery(galleryItems) {
+  return galleryItems
+    .map(
+      ({ original, preview, description }) =>
+        `<a class="gallery__item" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}"/>
+    </a>`
+    )
+    .join('');
+}
+
+galleryWrap.insertAdjacentHTML('beforeend', galleryCollection);
+
+// ------------------funkcja do otwierania okna modalnego------------------
+const lightbox = new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionsData: 'alt',
+  captionsDelay: 250,
+});
