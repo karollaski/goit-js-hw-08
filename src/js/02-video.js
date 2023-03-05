4af95cf1c3ed3840a823665f2fc127220a1e3a30
karollaski <karol.laski994@gmail.com>
2023-03-05 16:57:04 +0100

    finished task-02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,14 @@
+import throttle from 'lodash.throttle';
+import Player from '@vimeo/player';
 
+const STORAGE_TIME = 'videoplayer-current-time';
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+
+const onPlay = function (event) {
+  localStorage.setItem(STORAGE_TIME, event.seconds);
+};
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+player.setCurrentTime(JSON.parse(localStorage.getItem(STORAGE_TIME)) || 0);
