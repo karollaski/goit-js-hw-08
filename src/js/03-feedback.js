52a161529f750c8e876bab1a340224f20a21311e
karollaski <karol.laski994@gmail.com>
2023-03-06 20:26:27 +0100

    finish task-03

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,5 +1,38 @@
 import throttle from 'lodash.throttle';
 
-const STORAGE_FORM = 'feedback-form-state';
-
 const form = document.querySelector('.feedback-form');
+const STORAGE_FORM = 'feedback-form-state';
+const dataObject = {};
+
+dataFormUpdate();
+
+form.addEventListener('input', throttle(dataFormSave, 500));
+form.addEventListener('submit', dataSendForm);
+
+function dataFormSave(event) {
+  event.preventDefault();
+  dataObject.email = form.elements.email.value;
+  dataObject.message = form.elements.message.value;
+  localStorage.setItem(STORAGE_FORM, JSON.stringify(dataObject));
+}
+
+function dataFormUpdate() {
+  const formData = localStorage.getItem(STORAGE_FORM);
+  try {
+    const formDataValue = JSON.parse(formData);
+    form.elements.email.value = formDataValue.email || '';
+    form.elements.message.value = formDataValue.message || '';
+  } catch (error) {
+    console.log('Empty localeStorage');
+  }
+}
+
+function dataSendForm(event) {
+  event.preventDefault();
+  dataObject.email = form.elements.email.value;
+  dataObject.message = form.elements.message.value;
+  console.log(dataObject);
+
+  localStorage.removeItem(STORAGE_FORM);
+  form.reset();
+}
